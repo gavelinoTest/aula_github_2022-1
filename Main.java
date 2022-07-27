88a21e7a0de75e2a25ece209456d056c189bf236
Guilherme Avelino <gavelino@gmail.com>
2022-07-27 16:21:33 -0300

    fix minor in issues #4 and #5

--- Main.java
+++ Main.java
@@ -4,15 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
-		int option = mainMenu.getSelection();
-		if (option == 1){
-			Menu contaMenu = new Menu("Menu Contas", Arrays.asList("Abri", "Alterar", "Fechar"));
-			System.out.println(contaMenu.getSelection() + " foi selecionada");
-		}
-		else {
-			System.out.println(mainMenu.getSelection() + " foi selecionada");
-			System.out.println("Fim");
-		}
+		int option = -1; 
+		do{
+			option = mainMenu.getSelection();
+
+			if (option == 1){
+				Menu contaMenu = new Menu("Menu Contas", Arrays.asList("Abri", "Alterar", "Fechar", "Retornar"));
+				System.out.println(contaMenu.getSelection() + " foi selecionada");
+			}
+			else {
+				System.out.println(mainMenu.getSelection() + " foi selecionada");
+				System.out.println("Fim");
+			}
+		} while (option!=4);
+		
 		
 	}
 
