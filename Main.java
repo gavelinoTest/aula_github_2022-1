b64d4fd484d80244a4b2bd927f9699dcddba6991
Guilherme Avelino <gavelino@gmail.com>
2022-07-27 16:08:27 -0300

    Implement task #4

--- Main.java
+++ Main.java
@@ -4,8 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
-		System.out.println(mainMenu.getSelection() + " foi selecionada");
-		System.out.println("Fim");
+		int option = mainMenu.getSelection();
+		if (option == 1){
+			Menu contaMenu = new Menu("Menu Contas", Arrays.asList("Abri", "Alterar", "Fechar"));
+			System.out.println(contaMenu.getSelection() + " foi selecionada");
+		}
+		else {
+			System.out.println(mainMenu.getSelection() + " foi selecionada");
+			System.out.println("Fim");
+		}
+		
 	}
 
-}
+}	
